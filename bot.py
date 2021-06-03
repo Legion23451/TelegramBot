f5b7b021c0c0ba310fef953c402de3af682f7072
Legion23451 <50868553+Legion23451@users.noreply.github.com>
2021-06-04 00:03:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,30 @@ import telebot
 from telebot import types
 import sqlite3
 from sqlite3 import Error
-
+from time import ctime
 
 bot = telebot.TeleBot('1792282194:AAHS45Vu6zVNPvDA2WMItgCiFLr1E8tNypw')
 
 conn = sqlite3.connect('course prj(flask)/shop.db', check_same_thread=False)
 cursor = conn.cursor()
 
-def db_table_val(user_id: int, username: str, first_name: str, last_name: str):
-	cursor.execute('INSERT INTO USERS (user_id, username, first_name, last_name) VALUES (?, ?, ?, ?)', (user_id, username, first_name, last_name))
+def create_tables():
+    cursor.execute('''CREATE TABLE IF NOT EXISTS USERS 
+                        (user_id INTEGER PRIMARY KEY NOT NULL,
+                        username TEXT,
+                        first_name TEXT,
+                        last_name TEXT,
+                        phone INTEGER,
+                        error TEXT,
+                        reg_date TEXT);''')
+    conn.commit()
+
+def db_table_val(user_id: int, username: str, first_name: str, last_name: str, phone: int, error: str, reg_date: str):
+	cursor.execute('INSERT INTO USERS (user_id, username, first_name, last_name, phone, error, reg_date) VALUES (?, ?, ?, ?, ?, ?, ?)', (user_id, username, first_name, last_name, phone, error, reg_date))
 	conn.commit()
 
-@bot.message_handler(commands=['db'])
+create_tables()
+@bot.message_handler(commands=['reg'])
 
 def get_nick(message):
     bot.send_message(message.from_user.id, "Введите Имя")
@@ -22,17 +34,30 @@ def get_nick(message):
 def get_name(message):
     global regFirst_name
     regFirst_name = message.text
-    global lasTname
     bot.send_message(message.from_user.id, "Введите фамилию")
     bot.register_next_step_handler(message, get_lastname)
-    
+
 def get_lastname(message):
+    global lasTname
     lasTname = message.text
-    bot.send_message(message.from_user.id, "Ваше имя и фамилия добавлены.")
+    bot.send_message(message.from_user.id, "Введите телефон без 8")
+    bot.register_next_step_handler(message, get_phone)
+    
+def get_phone(message):
+    global phonenumber
+    phonenumber = message.text
+    bot.send_message(message.from_user.id, "Опишите вашу проблему.")
+    bot.register_next_step_handler(message, get_eror)
+
+def get_eror(message):
+    global error_text
+    error_text = message.text
+    bot.send_message(message.from_user.id, "Ваша заявка принята.")
     us_id = message.from_user.id
     us_nick = message.from_user.first_name
-    db_table_val(user_id=us_id, username=us_nick, first_name=regFirst_name, last_name=lasTname)
-     
+    date = ctime()
+    db_table_val(user_id=us_id, username=us_nick, first_name=regFirst_name, last_name=lasTname, phone=phonenumber, error=error_text, reg_date=ctime())    
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
