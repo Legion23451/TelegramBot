339f70632dcfafb978a34eac8a3154490c0ccaa3
Legion23451 <50868553+Legion23451@users.noreply.github.com>
2021-06-04 16:53:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,63 +1,11 @@
 import telebot
-from telebot import types
-import sqlite3
-from sqlite3 import Error
 from time import ctime
-
-bot = telebot.TeleBot('')
-
+from telebot import types
+from test import *
 conn = sqlite3.connect('course prj(flask)/shop.db', check_same_thread=False)
 cursor = conn.cursor()
 
-def create_tables():
-    cursor.execute('''CREATE TABLE IF NOT EXISTS USERS 
-                        (user_id INTEGER PRIMARY KEY NOT NULL,
-                        username TEXT,
-                        first_name TEXT,
-                        last_name TEXT,
-                        phone INTEGER,
-                        error TEXT,
-                        reg_date TEXT);''')
-    conn.commit()
-
-def db_table_val(user_id: int, username: str, first_name: str, last_name: str, phone: int, error: str, reg_date: str):
-	cursor.execute('INSERT INTO USERS (user_id, username, first_name, last_name, phone, error, reg_date) VALUES (?, ?, ?, ?, ?, ?, ?)', (user_id, username, first_name, last_name, phone, error, reg_date))
-	conn.commit()
-
-create_tables()
-@bot.message_handler(commands=['reg'])
-
-def get_nick(message):
-    bot.send_message(message.from_user.id, "Введите Имя")
-    bot.register_next_step_handler(message, get_name)
-
-def get_name(message):
-    global regFirst_name
-    regFirst_name = message.text
-    bot.send_message(message.from_user.id, "Введите фамилию")
-    bot.register_next_step_handler(message, get_lastname)
-
-def get_lastname(message):
-    global lasTname
-    lasTname = message.text
-    bot.send_message(message.from_user.id, "Введите телефон без 8")
-    bot.register_next_step_handler(message, get_phone)
-    
-def get_phone(message):
-    global phonenumber
-    phonenumber = message.text
-    bot.send_message(message.from_user.id, "Опишите вашу проблему.")
-    bot.register_next_step_handler(message, get_eror)
-
-def get_eror(message):
-    global error_text
-    error_text = message.text
-    bot.send_message(message.from_user.id, "Ваша заявка принята.")
-    us_id = message.from_user.id
-    us_nick = message.from_user.first_name
-    date = ctime()
-    db_table_val(user_id=us_id, username=us_nick, first_name=regFirst_name, last_name=lasTname, phone=phonenumber, error=error_text, reg_date=ctime())    
-
+bot = telebot.TeleBot('1792282194:AAHS45Vu6zVNPvDA2WMItgCiFLr1E8tNypw')
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
@@ -95,4 +43,43 @@ def get_text_messages(message):
     else:
         bot.send_message(message.from_user.id, 'Не понимаю ваш вопрос.')
 
-bot.polling(none_stop=True)
+@bot.message_handler(commands=['reg'])
+def chek_user(message):
+    teg_id = message.chat.id
+    cursor.execute(f"SELECT user_id FROM USERS WHERE user_id = {teg_id}")
+    data = cursor.fetchone()
+    if data is None:
+        bot.register_next_step_handler(message, get_nick)
+    else:
+        bot.send_message(message.from_user.id, "Вы уже зарегестрировали свою анкету.")
+        
+def get_nick(message):
+    bot.send_message(message.from_user.id, "Введите Имя")
+    bot.register_next_step_handler(message, get_name)
+
+def get_name(message):
+    regFirst_name = message.text
+    bot.send_message(message.from_user.id, "Введите фамилию")
+    bot.register_next_step_handler(message, get_lastname)
+    return regFirst_name
+
+def get_lastname(message):
+    lasTname = message.text
+    bot.send_message(message.from_user.id, "Введите телефон без 8")
+    bot.register_next_step_handler(message, get_phone)
+    return lasTname
+
+def get_phone(message):
+    phonenumber = message.text
+    bot.send_message(message.from_user.id, "Опишите вашу проблему.")
+    bot.register_next_step_handler(message, get_eror)
+    return phonenumber
+
+def get_eror(message):
+    error_text = message.text
+    bot.send_message(message.chat.id, "Ваша заявка принята.")
+    us_id = message.chat.id
+    us_nick = message.from_user.first_name
+    db_table_val(user_id=us_id, username=us_nick, first_name=get_name, last_name=get_lastname, phone=get_phone, error=error_text, reg_date=ctime())    
+
+bot.polling(none_stop=True, interval=0)
