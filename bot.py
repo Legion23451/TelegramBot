50a663c94832b059a433a96f7e0968217a70cde8
Legion23451 <50868553+Legion23451@users.noreply.github.com>
2021-06-06 03:31:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 import telebot
 from time import ctime
 from telebot import types
+from telebot.apihelper import get_chat_member
 from sql import *
-
-f = open('config.txt')
-TOKEN = f.readline()
-bot = telebot.TeleBot('')
+conn = sqlite3.connect('shop.db', check_same_thread=False)
+cursor = conn.cursor()
+f = open('config.txt', 'r')
+TOKEN = f.read()
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_start(message):
@@ -33,43 +35,36 @@ def url(message):
     markup = types.InlineKeyboardMarkup()
     btn_my_site= types.InlineKeyboardButton(text='Наш сайт', url='http://127.0.0.1:5000/')
     markup.add(btn_my_site)
-    bot.send_message(message.chat.id, "Нажми на кнопку и перейдите на наш сайт.", reply_markup = markup)
+    bot.send_message(message.chat.id, "Нажми на кнопку и перейдите на наш сайт.", reply_markup = markup)  
 
-@bot.message_handler(commands=['Анкета'])
-def chek_user(message):
-    teg_id = message.chat.id
-    cursor.execute(f"SELECT user_id FROM USERS WHERE user_id = {teg_id}")
-    data = cursor.fetchone()
-    if data is None:
-        bot.register_next_step_handler(message, get_nick)
+@bot.message_handler(commands=['reg'])
+def get_reg(message):
+    bot.send_message(message.from_user.id,'Введите Login')
+    bot.register_next_step_handler(message, get_login)
+
+def get_login(message):
+    global login_user
+    login_user = message.text
+    cursor.execute("SELECT login FROM user where login = ?", (login_user,))
+    log = cursor.fetchone()
+    logo = log[0]
+    if logo == login_user:   
+        bot.send_message(message.from_user.id, "Login уже используется, введите новый")
+        bot.register_next_step_handler(message, get_reg(message))
     else:
-        bot.send_message(message.from_user.id, "Вы уже зарегестрировали свою анкету.")
-        
-def get_nick(message):
-    bot.send_message(message.from_user.id, "Введите Имя")
-    bot.register_next_step_handler(message, get_name)
+        bot.send_message(message.from_user.id,'Введите Пароль')
+        bot.register_next_step_handler(message, get_pass)
 
-def get_name(message):
-    regFirst_name = message.text
-    bot.send_message(message.from_user.id, "Введите фамилию")
-    bot.register_next_step_handler(message, get_lastname)
+def get_pass(message):
+    global pass_user
+    pass_user = message.text
+    bot.register_next_step_handler(message, get_insert(message))
 
-def get_lastname(message):
-    lasTname = message.text
-    bot.send_message(message.from_user.id, "Введите телефон без 8")
-    bot.register_next_step_handler(message, get_phone)
-
-def get_phone(message):
-    phonenumber = message.text
-    bot.send_message(message.from_user.id, "Опишите вашу проблему.")
-    bot.register_next_step_handler(message, get_eror)
-
-def get_eror(message):
-    error_text = message.text
-    bot.send_message(message.chat.id, "Ваша заявка принята.")
-    us_id = message.chat.id
-    us_nick = message.from_user.first_name
-    db_table_val(user_id=us_id, username=us_nick, first_name=get_name, last_name=get_lastname, phone=get_phone, error=error_text, reg_date=ctime())  
+def get_insert(message):
+    userp = ("")
+    userinf = ("") 
+    insert_db(id = 19569, login = login_user, password = pass_user, userpic = userp, userinfo = userinf)
+    bot.send_message(message.from_user.id,'Вы успешно зарегестрировались в нашей системе')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -80,5 +75,6 @@ def get_text_messages(message):
         bot.send_message(message.chat.id, 'Как отменить заказ?')
     else:
         bot.send_message(message.from_user.id, 'Не понимаю ваш вопрос.')
+
 
 bot.polling(none_stop=True)
